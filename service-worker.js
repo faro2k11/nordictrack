bfac5426051c7ee6a647d457756ec4d79912e024
faro2k11 <fabian.rohde@outlook.com>
2026-06-04 14:32:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // Strategie: Cache-First für statische Assets, alle anderen Requests via Netz.
 // =============================================================================
 
-const CACHE_NAME = "t65s-v2.7";
+const CACHE_NAME = "t65s-v3.0";
 const PRECACHE_URLS = [
   "./",
   "./index.html",
